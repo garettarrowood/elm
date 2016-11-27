bea7fd05420a5ed1721738236dec5f5e8e3ecef6
Garett Arrowood <garettarrowood@gmail.com>
2016-11-27 16:07:32 -0500

    Complete 02 exercise1

---
+++ 02 more on functions/Exercise1.elm
@@ -0,0 +1,11 @@
+module Main exposing (..)
+
+import Html
+import String exposing (..)
+import List exposing (..)
+
+(~=) a b =
+    left 1 a == left 1 b
+
+main =
+    "bob" ~= "bend" |> toString |> Html.text
