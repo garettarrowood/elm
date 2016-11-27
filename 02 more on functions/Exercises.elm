ebb10ec2d9e44fe363cff646eb8ae4ae3dda11b4
Garett Arrowood <garettarrowood@gmail.com>
2016-11-27 16:14:45 -0500

    Add wordCount function to 02 Exercises

--- 02 more on functions/Exercises.elm
+++ 02 more on functions/Exercises.elm
@@ -7,5 +7,8 @@ import List exposing (..)
 (~=) a b =
     left 1 a == left 1 b
 
+wordCount sentence =
+    words sentence |> List.length
+
 main =
-    (~=) "bob" "bend" |> toString |> Html.text
+    wordCount "The toys are made in China." |> toString |> Html.text
